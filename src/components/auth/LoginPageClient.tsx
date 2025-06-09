54766d705167bad73c8d5dda6f0007383ca07646
fakerdeft <whakswp123@naver.com>
2025-06-09 15:58:05 +0900

    (#14)feat: 로컬/운영 환경 분리, 서브모듈, CI/CD 설정

--- src/components/auth/LoginPageClient.tsx
+++ src/components/auth/LoginPageClient.tsx
@@ -15,7 +15,7 @@ export default function LoginPageClient() {
   // 카카오 로그인 처리 함수
   const handleKakaoLogin = () => {
     const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
-    const REDIRECT_URI = `${window.location.origin}/api/auth/kakao/callback`;
+    const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
 
     // 팝업 창 크기 설정
     const width = 500;
